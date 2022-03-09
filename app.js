cfc999586229620de90f3716dfec7dee42d86d0a
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-09 15:00:09 +0200

    database connections MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const config = require('config')
 
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => {console.log(`App has been started on port ${PORT}`)})
+async function start () {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        app.listen(PORT, () => {console.log(`App has been started on port ${PORT}`)})
+    } catch (error) {
+        console.log('Server error', error.message)
+        process.exit(1)
+    }
+}
+
+start()
