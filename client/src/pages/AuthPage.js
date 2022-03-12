32614f31af19c3be4f74a0e3f7f65dc6be5f2966
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-12 10:57:16 +0200

    connect Front-end with api for registration

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,44 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHttp} from "../hook/http.hook";
 
 export const AuthPage = () => {
+    const {loading, request, error, clearError} = useHttp()
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            // message(data.message)
+            console.log('Data', data)
+        } catch (e) {
+        }
+    }
     return (
-        <div>
-            <div className="row">
-                <div className="col s6 offset-s3">
-                    <h1>Auth Page</h1>
-                    <div className="card blue darken-1">
-                        <div className="card-content white-text">
-                            <span className="card-title">Authorization</span>
-                            <div>
-                                <div className="input-field">
-                                    <input
-                                        placeholder="Write your Email"
-                                        id="email"
-                                        type="text"
-                                        className="yellow-input"
-                                        email='password'
-
-                                    />
-                                    <label htmlFor="email">Email</label>
-                                </div>
-
-                                <div className="input-field">
-                                    <input
-                                        placeholder="Write your Password"
-                                        id="password"
-                                        type="password"
-                                        className="yellow-input"
-                                        name='password'
-                                    />
-                                    <label htmlFor="email">Password</label>
-                                </div>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Auth Page</h1>
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Write your Email"
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    className="yellow-input"
+                                    value={form.email}
+                                    email='password'
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Write your Password"
+                                    id="password"
+                                    type="password"
+                                    className="yellow-input"
+                                    name='password'
+                                    value={form.password}
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Password</label>
                             </div>
                         </div>
-                        <div className="card-action">
-                            <button className="btn yellow darken-4" style={{marginRight: 10}}>Log in</button>
-                            <button className="btn grey lighten-1 black-text">Registration</button>
-                        </div>
-
+                    </div>
+                    <div className="card-action">
+                        <button className="btn yellow darken-4" style={{marginRight: 10}}>Log in</button>
+                        <button
+                            className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Registration
+                        </button>
                     </div>
                 </div>
             </div>
